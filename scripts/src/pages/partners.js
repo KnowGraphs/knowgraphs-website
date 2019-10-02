b51c7389fb24526bd26677e106a6d5e2ff7838dc
Tim Ermilov <yamalight@gmail.com>
2019-10-02 15:14:53 +0200

    Show beneficiaries before associated partners

--- scripts/src/pages/partners.js
+++ scripts/src/pages/partners.js
@@ -20,26 +20,28 @@ export default function Partners({
         <div className="container content">
           <h1>Partners</h1>
 
-          {Object.keys(partnersByType).map(type => (
-            <div
-              className="tile is-vertical"
-              style={{ marginBottom: '3em' }}
-              key={type}
-            >
-              <h2 style={{ marginBottom: '1em' }}>{type}</h2>
-              <div className="columns is-multiline is-5 is-variable">
-                {partnersByType[type]
-                  .sort((a, b) =>
-                    a.node.data.name.localeCompare(b.node.data.name)
-                  )
-                  .map(({ node }) => (
-                    <div className="column is-one-third" key={node.id}>
-                      <Partner partner={node} />
-                    </div>
-                  ))}
+          {Object.keys(partnersByType)
+            .sort((a, b) => b.localeCompare(b))
+            .map(type => (
+              <div
+                className="tile is-vertical"
+                style={{ marginBottom: '3em' }}
+                key={type}
+              >
+                <h2 style={{ marginBottom: '1em' }}>{type}</h2>
+                <div className="columns is-multiline is-5 is-variable">
+                  {partnersByType[type]
+                    .sort((a, b) =>
+                      a.node.data.name.localeCompare(b.node.data.name)
+                    )
+                    .map(({ node }) => (
+                      <div className="column is-one-third" key={node.id}>
+                        <Partner partner={node} />
+                      </div>
+                    ))}
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
         </div>
       </section>
     </Layout>
