eb5a3bc42535141a4a2081ae7518812fa19f9571
melissadas <melissadas22@gmail.com>
2020-03-29 15:27:56 +0200

    Description text changed and image updated.

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,14 @@ module.exports = {
     title: `KnowGraphs`,
     headline: '',
     siteName: 'KnowGraphs',
-    description: `The overall objective of KnowGraphs is to scale KGs to be accessible to a wide audience of users across multiple domains including (1) companies (in domains including Industry 4.0, bio-medicine, finance, law) of all sizes and even (2) end users (e.g., through personal assistants and web search).`,
+    description: `Knowledge graphs(KGs) are a flexible knowledge representation paradigm intended to allow knowledge to be consumed by humans and machines.KGs are widely regarded as a key enabler for a number of increasingly popular technologies including Web search, question answering, personal assistants and AI across most sectors including Industry 4.0, personalized medicine, legislation, economics and more.KGs are now used by several large companies as a key component of their data products.However, while they are rightly praised as a key technology for all future data- driven enterprises and regarded as a promising approach towards "blurring the lines between human and machine", KGs are currently unattainable for the majority of companies and users.
+
+    The objective of KnowGraphs is to scale KGs to be accessible to a wide [audience]() of users across multiple domains including companies(in domains including Industry 4.0, biomedicine, finance, law) of all sizes and even end users (e.g., through personal assistants and web search).Addressing this goal demands a mix of works from the theoretical foundations to the exploitation and economic repercussions of knowledge graphs. KnowGraphs focuses on addressing four of the facets of KG management: representation, construction and maintenance, operation and exploitation.
+    
+    The KnowGraphs team will address these pillars by researching and developing novel methods, models and frameworks using a cross-disciplinary mix of methods from Web Science, Data Science, Knowledge Representation, Knowledge Engineering, Big Data, data law, and business innovation. The consortium will apply its results across sectors, i.e., in domains including personalized medicine, Industry 4.0, question answering, personal assistants and Web search.
+    
+    Do not hesitate to contact us if you want to know more or are interested in collaborating with us.
+    `,
     author: `@dice-group`,
     footer: `This project has received funding from the European Union’s Horizon 2020 research and innovation programme under the Marie Skłodowska-Curie grant agreement No 860801.`,
     social: {
